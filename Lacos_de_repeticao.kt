a60b8bc5169da25286a65534b62e80a5a6206681
Rômulo Bovi <58385939+romulobovi@users.noreply.github.com>
2023-07-28 15:09:11 -0300

    lacos_de_repeticao.kt

---
+++ Lacos_de_repeticao.kt
@@ -0,0 +1,21 @@
+kotlin
+fun main() {
+    // Laço for
+    for (i in 1..5) {
+        println("Número: $i")
+    }
+
+    // Laço while
+    var j = 1
+    while (j <= 5) {
+        println("Número: $j")
+        j++
+    }
+
+    // Laço do-while
+    var k = 1
+    do {
+        println("Número: $k")
+        k++
+    } while (k <= 5)
+}
